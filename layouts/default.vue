e6aa7e5ef9bf3b9ff97af3d0e1214f4bf0353f02
Supakit <supakit.pnm@gmail.com>
2022-07-06 10:47:03 +0700

    Create: Header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,34 @@
 <template>
-    <div>
-        <nuxt></nuxt>
-    </div>
+  <div>
+    <TheHeader @sidenavToggle="displaySideNav = !displaySideNav" />
+    <TheSidenav :show="displaySideNav" @close="displaySideNav = false" />
+    <nuxt></nuxt>
+  </div>
 </template>
+
+<script>
+import TheHeader from "~/components/Navigation/TheHeader.vue";
+import TheSidenav from "~/components/Navigation/TheSidenav.vue";
+
+export default {
+  components: {
+    TheHeader,
+    TheSidenav,
+  },
+  data() {
+    return {
+      displaySideNav: false,
+    };
+  },
+};
+</script>
 
 <style>
 html {
-    font-family: 'Open Sans', 'sans-serif';
+  font-family: "Open Sans", "sans-serif";
 }
 
 body {
-    margin: 0;
+  margin: 0;
 }
 </style>
